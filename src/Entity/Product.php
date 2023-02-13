0fef1c276bbf0fb68a9aedca4ec16a62e7731b33
hamza <hamzamanai.prof@gmail.com>
2023-02-13 16:10:22 +0100

    adding user, product and category entities

---
+++ src/Entity/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
